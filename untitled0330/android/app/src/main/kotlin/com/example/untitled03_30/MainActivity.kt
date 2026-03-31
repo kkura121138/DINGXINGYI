6d68c738a4bcf140f2674c4e8886b720c92bec7c
DINGXINGYI <1604492619@qq.com>
2026-03-31 12:26:02 +0900

    123

---
+++ untitled0330/android/app/src/main/kotlin/com/example/untitled03_30/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.untitled03_30
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ untitled0330/lib/dingxingyi.dart
@@ -0,0 +1,5 @@
+void main(){
+  var lastName="Hong";
+  var firstName="Gildong";
+  print("Name is $lastName $firstName");
+}
